f13a101123cbba2e6ef242b33032045db277aadb
Stefan Rimaila <stefan@rimaila.fi>
2024-08-08 23:42:01 +0300

    feat(scripts): create experimental operator for node normalization

--- scripts/na_selected_nodes.py
+++ scripts/na_selected_nodes.py
@@ -19,11 +19,74 @@ gamma = [
 
 
 class NA_OT_surimi_normalize_image_nodes(T.Operator):
-    bl_idname = 'surimi.normalize_image_nodes'
+    bl_idname = 'surimi.experimental_normalize_image_nodes'
+    bl_label = 'Normalize Image Nodes (EXPERIMENTAL)'
     bl_options = {'REGISTER', 'UNDO'}
 
     def execute(self, ctx: T.Context):
-        return {'CANCELLED'}
+        node_rna_types = {'ShaderNodeOctImageTex', 'OctaneRGBImage'}
+
+        obj = C.active_object
+        mat = obj.active_material
+        mat_name = mat.name
+        nodes = mat.node_tree.nodes
+
+        done_count = 0
+
+        selected: list[T.Node] = \
+            [n
+             for n in nodes
+             if n.select
+             and n.bl_rna.identifier in node_rna_types]
+
+        if not len(selected):
+            self.report({'ERROR'}, 'No nodes selected to handle')
+            return {'CANCELLED'}
+
+        for n in selected:
+            img: T.Image = n.image
+            img_name = img.name
+
+            if not img_name.startswith(mat_name):
+                print(
+                    f'Image name `{img_name=}` does not start with `{mat_name=}`, skipping.')
+                continue
+
+            tail = img_name.removeprefix(f'{mat_name}_')
+            tail = re.match(r'(\w+)', tail)
+
+            if not tail:
+                print('Something silly going on')
+                print(f' - {img_name=}')
+                print(f' - {mat_name=}')
+                print(f' - {tail=}')
+                continue
+
+            tail = tail.group(1)
+            n.image.name = '-'.join([mat_name, tail])
+            n.label = tail
+            n.name = f'NODE-{n.image.name}'
+
+            # This only works if you have Octane
+            # Essentially should change the colorspace if in Cycles/Eevee
+            for types, gamma_value in gamma:
+                socket: T.NodeSocket
+
+                try:
+                    socket = n.inputs['Legacy gamma']
+                except KeyError:
+                    socket = n.inputs['Gamma']
+
+                if tail in types or tail.capitalize() in types:
+                    socket.default_value = gamma_value
+                else:
+                    socket.default_value = 1.0
+
+                done_count += 1
+
+        self.report({'INFO'}, f'Normalized {done_count} node(s).')
+
+        return {'FINISHED'}
 
 
 #
@@ -83,5 +146,14 @@ def main():
                 socket.default_value = 1.0
 
 
+def register():
+    bpy.utils.register_class(NA_OT_surimi_normalize_image_nodes)
+
+
+def unregister():
+    bpy.utils.unregister_class(NA_OT_surimi_normalize_image_nodes)
+
+
 if __name__ == '__main__':
-    main()
+    # main()
+    register()
